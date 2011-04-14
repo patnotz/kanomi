7064129ddf0d55c9d4783b2a47017bf329d97a67
Pat Notz <patnotz@gmail.com>
2011-04-14 13:30:36 -0600

    initial doxygen input

---
+++ navtree.js
@@ -0,0 +1,337 @@
+var NAVTREE =
+[
+  [ "Kanomi", "index.html", [
+    [ "Class List", "annotated.html", [
+      [ "kanomi::A", "structkanomi_1_1_a.html", null ],
+      [ "kanomi::enabled_pred< FIELD >::apply< IMPL >", "structkanomi_1_1enabled__pred_1_1apply.html", null ],
+      [ "kanomi::provides_field< FIELD >::apply< IMPL >", "structkanomi_1_1provides__field_1_1apply.html", null ],
+      [ "kanomi::B", "structkanomi_1_1_b.html", null ],
+      [ "kanomi::Base", "structkanomi_1_1_base.html", null ],
+      [ "kanomi::C", "structkanomi_1_1_c.html", null ],
+      [ "kanomi::ConstantScalar< FIELD, MODEL, STENCIL >", "structkanomi_1_1_constant_scalar.html", null ],
+      [ "kanomi::D", "structkanomi_1_1_d.html", null ],
+      [ "kanomi::DoEvaluate", "structkanomi_1_1_do_evaluate.html", null ],
+      [ "kanomi::DoSetup< M >", "structkanomi_1_1_do_setup.html", null ],
+      [ "kanomi::enabled< FIELD, MODEL >", "structkanomi_1_1enabled.html", null ],
+      [ "kanomi::enabled_pred< FIELD >", "structkanomi_1_1enabled__pred.html", null ],
+      [ "kanomi::HEX", "structkanomi_1_1_h_e_x.html", null ],
+      [ "kanomi::HEX_Q8", "structkanomi_1_1_h_e_x___q8.html", null ],
+      [ "kanomi::is_same< A, B >", "structkanomi_1_1is__same.html", null ],
+      [ "kanomi::is_same< A, A >", "structkanomi_1_1is__same_3_01_a_00_01_a_01_4.html", null ],
+      [ "kanomi::Manager< S >", "structkanomi_1_1_manager.html", null ],
+      [ "kanomi::Model< field::THERMAL_CONDUCTIVITY, STENCIL >", "structkanomi_1_1_model_3_01field_1_1_t_h_e_r_m_a_l___c_o_n_d_u_c_t_i_v_i_t_y_00_01_s_t_e_n_c_i_l_01_4.html", null ],
+      [ "kanomi::Named< T >", "structkanomi_1_1_named.html", null ],
+      [ "kanomi::provider_of_field< M, FieldT >", "structkanomi_1_1provider__of__field.html", null ],
+      [ "kanomi::provides_field< FIELD >", "structkanomi_1_1provides__field.html", null ],
+      [ "kanomi::QUAD", "structkanomi_1_1_q_u_a_d.html", null ],
+      [ "kanomi::QUAD_Q4", "structkanomi_1_1_q_u_a_d___q4.html", null ],
+      [ "kanomi::ScalarField< FIELD, MODEL, STENCIL >", "structkanomi_1_1_scalar_field.html", null ],
+      [ "kanomi::ScalarPolynomial< FIELD, MODEL, STENCIL, VARIABLE >", "structkanomi_1_1_scalar_polynomial.html", null ],
+      [ "kanomi::TET", "structkanomi_1_1_t_e_t.html", null ],
+      [ "kanomi::TET_Q4", "structkanomi_1_1_t_e_t___q4.html", null ],
+      [ "kanomi::TRI", "structkanomi_1_1_t_r_i.html", null ],
+      [ "kanomi::TRI_Q3", "structkanomi_1_1_t_r_i___q3.html", null ]
+    ] ],
+    [ "Class Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "kanomi::provides_field< FIELD >::apply< IMPL >", "structkanomi_1_1provides__field_1_1apply.html", null ],
+      [ "kanomi::Base", "structkanomi_1_1_base.html", [
+        [ "kanomi::A", "structkanomi_1_1_a.html", null ],
+        [ "kanomi::B", "structkanomi_1_1_b.html", null ],
+        [ "kanomi::C", "structkanomi_1_1_c.html", null ],
+        [ "kanomi::D", "structkanomi_1_1_d.html", null ]
+      ] ],
+      [ "kanomi::ConstantScalar< FIELD, MODEL, STENCIL >", "structkanomi_1_1_constant_scalar.html", null ],
+      [ "kanomi::DoEvaluate", "structkanomi_1_1_do_evaluate.html", null ],
+      [ "kanomi::DoSetup< M >", "structkanomi_1_1_do_setup.html", null ],
+      [ "kanomi::enabled< FIELD, MODEL >", "structkanomi_1_1enabled.html", null ],
+      [ "kanomi::enabled< FIELD, IMPL::ModelT >", "structkanomi_1_1enabled.html", [
+        [ "kanomi::enabled_pred< FIELD >::apply< IMPL >", "structkanomi_1_1enabled__pred_1_1apply.html", null ]
+      ] ],
+      [ "kanomi::enabled_pred< FIELD >", "structkanomi_1_1enabled__pred.html", null ],
+      [ "kanomi::HEX", "structkanomi_1_1_h_e_x.html", null ],
+      [ "kanomi::HEX_Q8", "structkanomi_1_1_h_e_x___q8.html", null ],
+      [ "kanomi::is_same< A, B >", "structkanomi_1_1is__same.html", null ],
+      [ "kanomi::is_same< A, A >", "structkanomi_1_1is__same_3_01_a_00_01_a_01_4.html", null ],
+      [ "kanomi::Manager< S >", "structkanomi_1_1_manager.html", null ],
+      [ "kanomi::Model< field::THERMAL_CONDUCTIVITY, STENCIL >", "structkanomi_1_1_model_3_01field_1_1_t_h_e_r_m_a_l___c_o_n_d_u_c_t_i_v_i_t_y_00_01_s_t_e_n_c_i_l_01_4.html", null ],
+      [ "kanomi::Named< T >", "structkanomi_1_1_named.html", null ],
+      [ "kanomi::provider_of_field< M, FieldT >", "structkanomi_1_1provider__of__field.html", null ],
+      [ "kanomi::provides_field< FIELD >", "structkanomi_1_1provides__field.html", null ],
+      [ "kanomi::QUAD", "structkanomi_1_1_q_u_a_d.html", null ],
+      [ "kanomi::QUAD_Q4", "structkanomi_1_1_q_u_a_d___q4.html", null ],
+      [ "kanomi::ScalarField< FIELD, MODEL, STENCIL >", "structkanomi_1_1_scalar_field.html", null ],
+      [ "kanomi::ScalarPolynomial< FIELD, MODEL, STENCIL, VARIABLE >", "structkanomi_1_1_scalar_polynomial.html", null ],
+      [ "kanomi::TET", "structkanomi_1_1_t_e_t.html", null ],
+      [ "kanomi::TET_Q4", "structkanomi_1_1_t_e_t___q4.html", null ],
+      [ "kanomi::TRI", "structkanomi_1_1_t_r_i.html", null ],
+      [ "kanomi::TRI_Q3", "structkanomi_1_1_t_r_i___q3.html", null ]
+    ] ],
+    [ "File List", "files.html", [
+      [ "build/CMakeFiles/CompilerIdC/CMakeCCompilerId.c", null, null ],
+      [ "build/CMakeFiles/CompilerIdCXX/CMakeCXXCompilerId.cpp", null, null ],
+      [ "src/Kanomi_ConstantScalar.hpp", null, null ],
+      [ "src/Kanomi_Demangle.cpp", null, null ],
+      [ "src/Kanomi_Demangle.hpp", null, null ],
+      [ "src/Kanomi_FieldNames.hpp", null, null ],
+      [ "src/Kanomi_main.cpp", null, null ],
+      [ "src/Kanomi_Manager.hpp", null, null ],
+      [ "src/Kanomi_Model.hpp", null, null ],
+      [ "src/Kanomi_ModelNames.hpp", null, null ],
+      [ "src/Kanomi_Models_ThermalConductivity.hpp", null, null ],
+      [ "src/Kanomi_Named.hpp", null, null ],
+      [ "src/Kanomi_ScalarField.hpp", null, null ],
+      [ "src/Kanomi_ScalarPolynomial.hpp", null, null ],
+      [ "src/Kanomi_Stencils.cpp", null, null ],
+      [ "src/Kanomi_Stencils.hpp", null, null ],
+      [ "src/Kanomi_TypeInfo.hpp", null, null ],
+      [ "src/User_Config.hpp", null, null ]
+    ] ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+

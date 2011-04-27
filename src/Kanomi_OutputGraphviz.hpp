a05223f6799adea0b21d6292d1d0035a270bad0e
Pat Notz <patnotz@gmail.com>
2011-04-27 09:48:32 -0600

    cleanup OutputGraphviz

--- src/Kanomi_OutputGraphviz.hpp
+++ src/Kanomi_OutputGraphviz.hpp
@@ -1,33 +1,31 @@
 #ifndef KANOMI_OUTPUTGRAPHVIZ_HPP_
 #define KANOMI_OUTPUTGRAPHVIZ_HPP_
 
-#include <Kanomi_mpl.hpp>
-#include <Kanomi_provider.hpp>
 #include <iosfwd>
 
 namespace bf = boost::fusion;
 
 namespace kanomi {
 
-template <class MANAGER, class FROM, class PREREQ_LIST>
-struct OutputGraphvizEdge;
-
-template <class MANAGER, class FROM, class FieldT, class StencilT, class TAIL>
-struct OutputGraphvizEdge<MANAGER, FROM, bf::cons< bf::pair<FieldT, StencilT>, TAIL> > {
-  static void write(std::ostream & os) {
-    os << "  " << FROM::name() << " -> " << FieldT::name() << ";" << std::endl;
-    OutputGraphvizEdge<MANAGER,FROM,TAIL>::write(os);
-  }
-};
-
-template <class MANAGER, class FROM>
-struct OutputGraphvizEdge<MANAGER,FROM,nil> {
-  static void write(std::ostream & os) {}
-};
-
 template <class M>
 struct OutputGraphviz {
   typedef M ManagerT;
+private:
+  template <class MANAGER, class FROM, class PREREQ_LIST>
+  struct OutputGraphvizEdge;
+  template <class MANAGER, class FROM, class FieldT, class StencilT, class TAIL>
+  struct OutputGraphvizEdge<MANAGER, FROM, bf::cons< bf::pair<FieldT, StencilT>, TAIL> > {
+    static void write(std::ostream & os) {
+      os << "  " << FROM::name() << " -> " << FieldT::name() << ";" << std::endl;
+      OutputGraphvizEdge<MANAGER,FROM,TAIL>::write(os);
+    }
+  };
+  template <class MANAGER, class FROM>
+  struct OutputGraphvizEdge<MANAGER,FROM,nil> {
+    static void write(std::ostream & os) {}
+  };
+  std::ostream & ostream;
+public:
   OutputGraphviz(std::ostream & os) : ostream(os) {}
   template<class T>
   void operator()(T & t) const {
@@ -36,9 +34,7 @@ struct OutputGraphviz {
     ostream << "  " << FieldT::name() << " [shape=box,style=filled];" << std::endl;
     OutputGraphvizEdge<M,FieldT,PrereqsT>::write(ostream);
   }
-  std::ostream & ostream;
 };
-
 
 } // namespace kanomi
 
